95e27eacf53a72e7bb2e6932c173689a2501dd12
gx1285 <78240988+gx1285JP@users.noreply.github.com>
2021-08-09 15:58:21 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from os import getenv
+import traceback
+from server import keep_alive
+
+prefix = getenv('discord_prefix')
+
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    print("Botの名前")
+    print(bot.user.name)  
+    print("BotのID")
+    print(bot.user.id)  
+    print("Discord.pyのバージョン")
+    print(discord.__version__)
+    await bot.change_presence(activity=discord.Game(name=f"TEST"))#サーバー数を表示するには{len(bot.guilds)} ユーザー数を表示するなら{len(bot.users)}
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+    
+keep_alive()
+bot.run(getenv('discord_token'))
